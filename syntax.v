482a176e3d7ebc42d11ded012309d767dbf57a9c
Burca Rafael <39907750+Cr34TIVv3@users.noreply.github.com>
2020-12-17 18:09:29 +0200

    Update syntax.v

--- syntax.v
+++ syntax.v
@@ -1,25 +1,32 @@
 (*
 switch
-
 lambda
-
 tipuri de variabile
-
-
 functii + functii de conversie intre tipuri de date
-
-
 clase
-
-
 tablouri
 *)
+
 
 (*Sintaxa cod*)
 Require Import Strings.String.
 Local Open Scope string_scope.
 Local Open Scope list_scope.
 Scheme Equality for string.
+
+(* Inductive Array_value :=
+(* | error_array : Array_value *)
+| empty_array : Array_value
+(* | start_array : Z->Array_value *)
+| Carray : Array_value -> nat -> Array_value.
+
+Definition myarray := empty_array.
+Definition mynewarray := Carray myarray 5.
+
+
+Compute empty_array.
+Compute Carray mynewarray 25. *)
+
 
 (*ERRnat si ERRbool sunt pentru situatiile in care pot aparea erori *)
 
@@ -70,6 +77,7 @@ Notation "A  b<= B" := (ble  A B) (at level 80).
 Notation "A  b>  B" := (bgt  A B) (at level 80).
 Notation "A  b>= B" := (bge  A B) (at level 80).
 Notation "b!     A" := (bnot   A) (at level 77, left associativity).
+Notation "A  b=  B" := (beq  A B) (at level 80).
 Notation "A  b&& B"  := (band A B) (at level 78, left associativity).
 Notation "A  b|| B"  := (bor  A B) (at level 79, left associativity).
 Coercion bconst: ERRbool >-> BExp.
@@ -81,9 +89,9 @@ Notation "A  s+  B" := (strcat A B) (at level 82).
 Notation "A  s*  n" := (strtimes A n) (at level 81).
 Coercion strconst: ERRstring >-> StrExp.
 Inductive Stmt :=
-| declNat:    string -> AExp   -> Stmt
-| declBool:   string -> BExp   -> Stmt
-| declStr:    string -> StrExp -> Stmt
+| declNat_l:  string -> AExp   -> Stmt
+| declBool_l: string -> BExp   -> Stmt
+| declStr_l:  string -> StrExp -> Stmt
 
 | assignNat:  string -> AExp   -> Stmt
 | assignBool: string -> BExp   -> Stmt
@@ -92,25 +100,58 @@ Inductive Stmt :=
 | sequence:   Stmt   -> Stmt   -> Stmt
 | whiledo:    BExp   -> Stmt   -> Stmt
 | fordo:      Stmt   -> BExp   -> Stmt -> Stmt -> Stmt
-| ifthen:     BExp   -> Stmt   -> Stmt
+
 | ifelse:     BExp   -> Stmt   -> Stmt -> Stmt
+| ifthen:     BExp   -> Stmt   -> Stmt
+
+| switch:     AExp   -> Stmt   -> Stmt
+| case:       AExp   -> Stmt   -> Stmt
 
 | break:      Stmt
 | continue:   Stmt
 
-| 
-Notation "X nat:=  A"                      := (assignNat X A)   (at level 100).
-Notation "X bool:= A"                      := (assignBool X A)  (at level 100).
-Notation "'NATURAL' X ::= A"               := (declNat X A)     (at level 100).
-Notation "'BOOLEAN' X ::= A"               := (declBool X A)    (at level 100).
+| callFun:    string -> Stmt   -> Stmt
+| callLambda: string -> Stmt   -> Stmt -> Stmt.
+
+Notation "'NATURAL' X := N"                := (declNat X N)     (at level 100).
+Notation "'BOOLEAN' X := B"                := (declBool X B)    (at level 100).
+Notation "'STRING'  X := S"                := (declStr X S)     (at level 100).
+Notation "X nat:=  N"                      := (assignNat X N)   (at level 100).
+Notation "X bool:= B"                      := (assignBool X B)  (at level 100).
+Notation "X str:=  S"                      := (assignStr X S)   (at level 100).
 Notation "S1 ;; S2"                        := (sequence S1 S2)  (at level 100, right associativity).
-Notation "'while' ( B ) { S }"             := (whiledo B S)     (at level 100).
+Notation "'while' ( B )  { S }"            := (whiledo B S)     (at level 100).
 Notation "'for' ( S1  B  S2 ) { S }"       := (fordo S1 B S2 S) (at level 100).
-Notation "'if' ( B ) { S }"                := (ifthen B S)      (at level 100).
 Notation "'if' ( B ) { S1 } 'else' { S2 }" := (ifelse B S1 S2)  (at level 100).
+Notation "'IF' ( B ) { S }"                := (ifthen B S)      (at level 100).
+Notation "'switch' ( A ) { S }"            := (switch A S)      (at level 100).
+Notation "'case' :: A    S"                := (case A S)        (at level 100).
+Notation "'call' N ( S1 )"                 := (callFun N S1)    (at level 100).
+
+Inductive Program :=
+| seqPrg:       Program -> Program -> Program
+| declMain:     Stmt    -> Program
+| declFun:      string  -> Stmt    -> Stmt     -> Program
+
+| declNat_g:    string  -> AExp   -> Program
+| declBool_g:   string  -> BExp   -> Program
+| declStr_g:    string  -> StrExp -> Program
+
+| declClass:    string  -> Stmt   -> Program.
+Notation "P1 P2"  := (seqPrg P1 P2) (at level 100).
+Notation "'MAIN' '()' { S }" := (declMain S) (at level 100).
+Notation "'function' N ( S ) { SS }" := (declFun N S SS) (at level 100).
+Notation "'GLOBAL' 'NATURAL' x"
+
+
+
+
+
+
+
 
 Inductive Result :=
-| subCode :         Stmt      -> Result (*functiile trebuie mapate cu codul lor*)
+| subCode :         Stmt    -> Result (*functiile trebuie mapate cu codul lor*)
 | errUndecl :       Result
 | errAssign :       Result
 | natResult :       ERRnat  -> Result
@@ -180,7 +221,6 @@ Configuratie
 
 
 Definition Stack := list Env.
-
 
 
 
